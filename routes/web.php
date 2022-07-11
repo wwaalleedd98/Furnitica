9107daf148ad12601a78f8c25bbbc58f37294ae1
Walid Yasin <walid.yasin98@outlook.com>
2022-07-12 00:12:00 +0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('vater');
+// });
+
+Route::prefix('/')->group(function(){
+    Route::view('/', 'cms.vater');
 });
